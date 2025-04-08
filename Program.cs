41f97504b6c4b09fe6d86ee426f2f0e790a6f2a4
Sunny Sahu <sunnysahu8210@gmail.com>
2025-04-08 19:24:42 +0530

    PORT Config Added in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Force port 5153 for example
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5153); // or your preferred port
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a068458d7658992984232bd5835163edb3e6554")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
